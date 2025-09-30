d5be2aea115088b880f18f3c16beeb9097a8330e
Ricardo Lima <ricardo.engp@gmail.com>
2025-09-30 15:20:15 -0300

    test: add unit tests for hasAccess middleware to validate role-based access control

---
+++ src/middlewares/hasAccess.middleware.test.ts
@@ -0,0 +1,26 @@
+import { hasAccess } from "./hasAccess.middleware";
+import { Request, Response } from "express";
+import { ForbiddenException } from "@exceptions/forbidden.exception";
+
+describe("HasAccessMiddleware", () => {
+    it("should be defined", () => {
+        expect(hasAccess).toBeDefined();
+    });
+
+    it("should throw a ForbiddenException if the user does not have the required role", () => {
+        const req = { session: { user: { role: "USER" } } } as Request;
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() } as unknown as Response;
+        const next = jest.fn();
+
+        expect(() => hasAccess(["ADMIN"])(req, res, next)).toThrow(ForbiddenException);
+    });
+
+    it("should call next() if the user has the required role", () => {
+        const req = { session: { user: { role: "ADMIN" } } } as Request;
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() } as unknown as Response;
+        const next = jest.fn();
+
+        hasAccess(["ADMIN"])(req, res, next);
+        expect(next).toHaveBeenCalled();
+    });
+});
