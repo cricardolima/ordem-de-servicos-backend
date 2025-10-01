c7e52927285d1b365bfb37b53c2aabf5c60618de
Ricardo Lima <ricardo.engp@gmail.com>
2025-10-01 00:59:01 -0300

    test: add unit tests for RefreshTokenUseCase to validate token generation, validation, and revocation

---
+++ src/use-cases/RefreshToken/RefreskToken.test.ts
@@ -0,0 +1,75 @@
+import { RefreshTokenUseCase } from "./RefreshToken.use-case";
+import { Container } from "inversify";
+import { InMemoryRefreshTokenRepository } from "@tests/repositories/InMemoryRefreshTokenRepository";
+import { TYPES } from "@container/types";
+import { IRefreshTokenRepository } from "@repositories/RefreshTokenRepository";
+import { ContainerApp } from "@container/inversify.config";
+
+describe('RefreshTokenUseCase', () => {
+    let container: Container;
+    let refreshTokenUseCase: RefreshTokenUseCase;
+    let inMemoryRefreshTokenRepository: InMemoryRefreshTokenRepository;
+
+    beforeEach(() => {
+        container = new ContainerApp().init();
+        container.unbind(TYPES.IRefreshTokenRepository);
+
+        inMemoryRefreshTokenRepository = new InMemoryRefreshTokenRepository();
+        container.bind<IRefreshTokenRepository>(TYPES.IRefreshTokenRepository).toConstantValue(inMemoryRefreshTokenRepository);
+        
+        refreshTokenUseCase = container.get<RefreshTokenUseCase>(TYPES.IRefreshTokenUseCase);
+
+        inMemoryRefreshTokenRepository.addRefreshToken({
+            id: '1',
+            token: 'refresh-token-1',
+            userId: '1',
+            expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 7),
+            createdAt: new Date(),
+            revokedAt: null,
+        });
+    });
+
+    afterEach(() => {
+        inMemoryRefreshTokenRepository.clear();
+    });
+
+    it('should be defined', () => {
+        expect(RefreshTokenUseCase).toBeDefined();
+    });
+
+    it('should generate a refresh token', async () => {
+        const refreshToken = await refreshTokenUseCase.generateRefreshToken('1');
+
+        expect(refreshToken).toBeDefined();
+        expect(refreshToken).not.toBeNull();
+        expect(refreshToken).toEqual(expect.objectContaining({
+            id: expect.any(String),
+            token: expect.any(String),
+            userId: '1',
+            expiresAt: expect.any(Date),
+            createdAt: expect.any(Date),
+            revokedAt: null,
+        }));
+    });
+
+    it('should validate a refresh token', async () => {
+        const refreshToken = await inMemoryRefreshTokenRepository.findByToken('refresh-token-1');
+        const validatedRefreshToken = await refreshTokenUseCase.validateRefreshToken(refreshToken.token);
+        
+        expect(validatedRefreshToken).toBeDefined();
+        expect(validatedRefreshToken).not.toBeNull();
+        expect(validatedRefreshToken).toEqual(expect.objectContaining({
+            tokenId: expect.any(String),
+            userId: expect.any(String),
+        }));
+    });
+
+    it('should revoke a refresh token', async () => {
+        const refreshToken = await inMemoryRefreshTokenRepository.findByToken('refresh-token-1');
+        await refreshTokenUseCase.revokeRefreshToken(refreshToken.token);
+        
+        expect(refreshToken).toBeDefined();
+        expect(refreshToken).not.toBeNull();
+        expect(refreshToken.revokedAt).not.toBeNull();
+    });
+});
